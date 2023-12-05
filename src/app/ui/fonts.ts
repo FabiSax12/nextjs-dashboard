aa78347baa5edb4884ba835b085acd17fe397604
Fabian Vargas <fabidev18@gmail.com>
2023-12-05 16:10:23 -0600

    first-commit

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Montserrat, Lusitana, Inter } from "next/font/google"
+
+export const montserrat = Montserrat({subsets: ["latin"], weight: ["400", "500", "600", "700"]})
+export const lusitana = Lusitana({subsets: ["latin"], weight: ["400", "700"]})
+export const inter = Inter({subsets: ["latin"]})
